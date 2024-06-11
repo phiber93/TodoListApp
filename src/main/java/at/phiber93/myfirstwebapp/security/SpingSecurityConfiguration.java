0da066159bf633cb2726551e2bc5f646fc896a4a
phiber93 <153130604+phiber93@users.noreply.github.com>
2024-06-11 13:27:02 +0200

    finished version

---
+++ src/main/java/at/phiber93/myfirstwebapp/security/SpingSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package at.phiber93.myfirstwebapp.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpingSecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails1 = createNewUser("phiber93", "dummy");
+        UserDetails userDetails2 = createNewUser("Bernhard", "dummydummy");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(password)
+                .roles("USER", "ADMIN")
+                .build();
+        return userDetails;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        httpSecurity.formLogin(withDefaults());
+
+        httpSecurity.csrf((csrf) -> csrf.disable());
+        httpSecurity.headers((headers) -> headers.disable());
+        return httpSecurity.build();
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'phiber93', 'Learn Spring Boot', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'phiber93', 'Learn Angular', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'phiber93', 'Learn to cook', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'Bernhard', 'Learn some stuff', CURRENT_DATE(), false);
